ac738cbba5f423856678275a86f2d1ec5b8611a3
rubens filipe <rubensfilipe6@gmail.com>
2024-10-07 13:31:54 -0300

    menu-mobile, menu fixo, dropdown

--- js/index.js
+++ js/index.js
@@ -24,3 +24,37 @@ let swiper = new Swiper(".slide-deposition", {
 AOS.init({
     duration:1000,
 });
+
+let btnDropdown = document.querySelector('.js-btn-dropdown')
+let dropdown = document.querySelector('.dropdown')
+let header = document.getElementById('js-header')
+let btnMobile = document.querySelector('.js-btn-mobile')
+let jsOverlayMenu = document.querySelector('.js-overlay')
+
+function openDropdown(e){
+  e.preventDefault()
+  dropdown.classList.toggle('active')
+}
+
+btnDropdown.addEventListener('click',openDropdown)
+dropdown.addEventListener('mouseleave',openDropdown)
+
+
+function fixedMenu(){
+  let scrollPage = window.scrollY
+  if( scrollPage > 80 ){
+    header.classList.add('fixed-menu')
+  }else{
+    header.classList.remove('fixed-menu')
+  }
+}
+document.addEventListener('scroll',fixedMenu)
+
+function openMenuMobile(){
+  document.documentElement.classList.toggle('menu-opened')
+  console.log(document)
+}
+
+btnMobile.addEventListener('click', openMenuMobile)
+
+jsOverlayMenu.addEventListener('click',openMenuMobile)
